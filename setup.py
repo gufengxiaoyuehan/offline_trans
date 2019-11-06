12530482bdae3fc055d8e6350a546085c186aef9
logan <chong.luo@cidatahub.com>
2019-11-06 17:16:49 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="offline-trans",
+    version="0.0.1",
+    description="transport docker iamges fast by only contain diff layers",
+    install_requires=[
+        'Click',
+    ],
+    packages=find_packages(include=['offline_trans']),
+    entry_points='''
+        [console_scripts]
+        offline_trans=offline_trans.cli:cli
+    '''
+)
